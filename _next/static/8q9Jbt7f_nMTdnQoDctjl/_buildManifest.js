567ab2cbe23bf179e887133e2c83e3212978de19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-09 18:46:56 +0000

    Deploy thawankeane/zeroquinze to github.com/thawankeane/zeroquinze.git:gh-pages

---
+++ _next/static/8q9Jbt7f_nMTdnQoDctjl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0586bcdb9b417e61115c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fc94b17185203b8a5864.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
